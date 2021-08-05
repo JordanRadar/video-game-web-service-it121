8628e3511cf50ac36b6b8e820bb30dc635c9220a
Jordan Radnich <jj.radnich@gmail.com>
2021-08-05 11:17:17 -0700

    I should probably put something useful in these comments

--- index.php
+++ index.php
@@ -62,6 +62,13 @@ function bondJSON(data){
 	//identifies the type of data returned
 	$('#filmtitle').html(data.title);
 
+	$.each(data.films,function(i,item){
+		let myFilm = bondTemplate(item);
+
+		$('<div></div>').html(myFilm).appendTo('#films');
+
+	});
+
 	//$("#output").text(JSON.stringify(data));
 
 	//this creates a map of the JSON on our page
@@ -100,6 +107,7 @@ function bondTemplate(film){
 		<a href="box" class="category">Bond Films By International Box Office Totals</a>
 		<h3 id="filmtitle">Title Will Go Here</h3>
 		<div id="films">
+			<!--
 			<div class="film">
 			<b>Film: </b> 1</br>
 			<b>Title: </b> Dr. No</br>
@@ -112,8 +120,8 @@ function bondTemplate(film){
 			<b>Budget: </b> $1,000,000.00</br>
 			<b>Box Office:</b> $59,567,035.00</br>
 			<div class="pic"><img src="thumbnails/dr-no.jpg" /></div>
-
-			</div>
+		</div>
+			-->
 		</div>
 		<div id="output">Results go here</div>
 	</body>
