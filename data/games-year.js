25ba36fa78fd2188791f568a62755b2ed0f7ea5c
Jordan Radnich <jj.radnich@gmail.com>
2021-08-11 10:52:46 -0700

    Adds games-year.js and some other stuff

--- data/games-year.js
+++ data/games-year.js
@@ -3,17 +3,44 @@
 	"games":[
 			  {
 				"Title":"Sonic the Hedgehog",
-				"Title":"Dr. No",
-				"Year":1962,
-				"Director":"Terence Young",
-				"Producers":"Harry Saltzman and Albert R. Broccoli",
-				"Writers":"Richard Maibaum, Johanna Harwood and Berkely Mather",
-				"Composer":"Monty Norman",
-				"Bond":"Sean Connery",
-				"Budget":"$1,000,000.00",
-				"BoxOffice":"$59,567,035.00",
-				"Image":"dr-no.jpg"
+				"Genre":"Platformer",
+				"Company":"Sega",
+				"Year":"1991",
+				"Rating":"E",
+				"Image":"sonic-the-hedgehog.jpg"
 			  },
+			  {
+				"Title":"Super Mario Kart",
+				"Genre":"Racing",
+				"Company":"Nintendo",
+				"Year":"1992",
+				"Rating":"E",
+				"Image":"super-mario-kart.jpg"
+			  },
+			  {
+				"Title":"Portal",
+				"Genre":"Puzzle",
+				"Company":"Valve",
+				"Year":"2007",
+				"Rating":"T",
+				"Image":"portal.jpg"
+			  },
+			  {
+				"Title":"Minecraft",
+				"Genre":"Sandbox",
+				"Company":"Mojang",
+				"Year":"2011",
+				"Rating":"E",
+				"Image":"minecraft.jpg"
+			  },
+			  {
+				"Title":"Counter-Strike: Global Offensive",
+				"Genre":"Tactical Shooter",
+				"Company":"Valve",
+				"Year":"2012",
+				"Rating":"M",
+				"Image":"counter-strike-global-offensive.jpg"
+			  }
 	]
 }
 
@@ -21,6 +48,7 @@
 Sonic The Hedgehog, Platformer, Sega, 1991, E
 Super Mario Kart, Racing, Nintendo, 1992, E
 Portal, Puzzle, Valve, 2007, T
+
 Minecraft, Sandbox, Mojang, 2011, E
 Counter-Strike Global Offensive, Tactical Shooter, Valve,2012,M
 
